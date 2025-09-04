42c28f7c127c82259dc9d50b315ddeb1866db4f8
Nam Van Nguyen <128123809+Beginlearncode@users.noreply.github.com>
2025-09-04 16:13:14 +0700

    start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+set -e
+
+# 1) Virtual display
+Xvfb ${DISPLAY} -screen 0 1280x800x24 -ac +extension RANDR &
+
+# 2) Window manager nhẹ
+fluxbox &
+
+# 3) VNC server trên cổng 5901
+if [ -n "${VNC_PASSWORD}" ]; then
+  x11vnc -display ${DISPLAY} -forever -shared -rfbport 5901 -passwd ${VNC_PASSWORD} &
+else
+  x11vnc -display ${DISPLAY} -forever -shared -rfbport 5901 -nopw &
+fi
+
+# 4) noVNC map $PORT (Render yêu cầu) -> VNC 5901
+/opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen ${PORT:-8080} &
+
+# 5) Chạy app Node
+npm start
