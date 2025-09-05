116c58dc860ee288d0ab500d677010d906143eaa
Nam Van Nguyen <128123809+Beginlearncode@users.noreply.github.com>
2025-09-05 09:02:31 +0700

    start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# 1) Màn hình ảo
+# 1) Virtual display
 Xvfb ${DISPLAY} -screen 0 1280x800x24 -ac +extension RANDR &
 
 # 2) Window manager nhẹ
 fluxbox &
 
-# 3) VNC server (5901)
+# 3) VNC server trên 5901
 if [ -n "${VNC_PASSWORD}" ]; then
   x11vnc -display ${DISPLAY} -forever -shared -rfbport 5901 -passwd ${VNC_PASSWORD} &
 else
   x11vnc -display ${DISPLAY} -forever -shared -rfbport 5901 -nopw &
 fi
 
-# 4) noVNC: map $PORT (Render yêu cầu) -> 5901
+# 4) noVNC: expose qua $PORT (Render yêu cầu)
 /opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen ${PORT:-8080} &
 
 # 5) Chạy app Node
